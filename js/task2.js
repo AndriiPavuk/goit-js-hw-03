b34515d96f31c29340296f98a65865a97d71f638
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-10-26 22:36:51 +0300

    new-way

--- js/task2.js
+++ js/task2.js
@@ -1,3 +1,5 @@
+// long way //
+
 function makeArray(firstArray, secondArray, maxLength) {
     const nextArray = [];
     for (let i = 0; i < maxLength; i++) {
@@ -20,3 +22,23 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+// fast way //
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//    let nextArray = firstArray.concat(secondArray);
+    
+//     if (nextArray.length > maxLength) {
+//         nextArray = nextArray.slice(0, maxLength);
+//     }
+
+//     return nextArray;
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
